b50712bacfe35deb728381905d5f151ee4b2ca10
Edward <soporte.tekniikan@gmail.com>
2026-09-04 10:13:59 -0500

    style: refine premium Natviewer homepage

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/es');
+
+Route::prefix('{locale}')
+    ->whereIn('locale', ['es', 'en'])
+    ->group(function () {
+        Route::get('/', function (string $locale) {
+            App::setLocale($locale);
+
+            return view('home', [
+                'locale' => $locale,
+            ]);
+        })->name('home');
+    });
